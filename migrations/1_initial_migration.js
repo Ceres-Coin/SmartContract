40e2afe8c1f6347c98402da4e9a0e8803dad25eb
stevenkcolin <84952580+IlyaPrigogine@users.noreply.github.com>
2021-05-28 13:02:18 +0800

    Update 1_initial_migration.js

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,16 @@
-const Migrations = artifacts.require("Migrations");
+// const Migrations = artifacts.require("Migrations");
 
-module.exports = function (deployer) {
+// module.exports = function (deployer) {
+//   deployer.deploy(Migrations);
+// };
+
+const Migrations = artifacts.require("Util/Migrations");
+
+module.exports = function(deployer, network, accounts) {
+
+  console.log("ACCOUNTS");
+  console.log(accounts);
+
   deployer.deploy(Migrations);
 };
+
