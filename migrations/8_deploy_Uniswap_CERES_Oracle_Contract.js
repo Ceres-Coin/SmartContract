2e48c0ea49c2ec85a02809216f7bcdb75f5633be
Ilya Prigogine <84952580+IlyaPrigogine@users.noreply.github.com>
2021-05-30 15:25:54 +0800

    Update 8_deploy_Uniswap_CERES_Oracle_Contract.js

--- migrations/8_deploy_Uniswap_CERES_Oracle_Contract.js
+++ migrations/8_deploy_Uniswap_CERES_Oracle_Contract.js
@@ -38,15 +38,30 @@ const SwapToPrice = artifacts.require("Uniswap/SwapToPrice");
 const CEREStable = artifacts.require("Ceres/CEREStable");
 const CEREShares = artifacts.require("CSS/CEREShares");
 
+// UniswapPairOracle
 const UniswapPairOracle_CERES_WETH = artifacts.require("Oracle/Variants/UniswapPairOracle_CERES_WETH");
 const UniswapPairOracle_CERES_USDC = artifacts.require("Oracle/Variants/UniswapPairOracle_CERES_USDC");
 const UniswapPairOracle_CSS_WETH = artifacts.require("Oracle/Variants/UniswapPairOracle_CSS_WETH");
 const UniswapPairOracle_CSS_USDC = artifacts.require("Oracle/Variants/UniswapPairOracle_CSS_USDC");
 
+// Uniswap Contract
+const Timelock = artifacts.require("Governance/Timelock");
+
 
 
 // Make sure Ganache is running beforehand
 module.exports = async function(deployer, network, accounts) {
+
+	// Uniswap Address
+	let uniswapFactoryInstance;
+	let ceresInstance;
+	let cssInstance;
+	let wethInstance;
+	let col_instance_USDC;
+	let col_instance_USDT;
+	let col_instance_6DEC;
+	let timelockInstance;
+	let routerInstance;
 
 	// set the deploy address
 	console.log(chalk.yellow('===== SET THE DEPLOY ADDRESSES ====='));
@@ -66,6 +81,35 @@ module.exports = async function(deployer, network, accounts) {
 	console.log("IS_ROPSTEN: ",IS_ROPSTEN);
 	console.log("IS_RINKEBY: ",IS_RINKEBY);
 	console.log("IS_DEV: ",IS_DEV);
+
+	if (IS_DEV) {
+		uniswapFactoryInstance = await UniswapV2Factory.deployed(); 
+		ceresInstance = await CEREStable.deployed();
+		cssInstance = await CEREShares.deployed();
+		wethInstance = await WETH.deployed();
+		col_instance_USDC = await FakeCollateral_USDC.deployed(); 
+		col_instance_USDT = await FakeCollateral_USDT.deployed(); 
+		col_instance_6DEC = await FakeCollateral_6DEC.deployed();
+		timelockInstance = await Timelock.deployed();
+		routerInstance = await UniswapV2Router02_Modified.deployed(); 
+	}
+	
+
+
+	// get uniswapFactory Instance * ceres/css instance;
+
+	// ======== Set the Uniswap oracles ========
+	console.log(chalk.yellow('========== UNISWAP ORACLES =========='));
+	console.log(chalk.blue('=== CERES ORACLES ==='));
+	await Promise.all([
+		// deployer.deploy(UniswapPairOracle_CERES_WETH, uniswapFactoryInstance.address, ceresInstance.address, wethInstance.address, COLLATERAL_CERES_AND_CERESHARES_OWNER, timelockInstance.address),
+		// deployer.deploy(UniswapPairOracle_CERES_USDC, uniswapFactoryInstance.address, ceresInstance.address, col_instance_USDC.address, COLLATERAL_CERES_AND_CERESHARES_OWNER, timelockInstance.address),
+		
+		
+	]);
+
+
+	
 
 
 
