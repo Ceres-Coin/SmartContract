8daba3c7fef101cd3321a459a81e7d715ffd6ea2
Ilya Prigogine <84952580+IlyaPrigogine@users.noreply.github.com>
2021-05-30 08:10:07 +0800

    add fake USDC & USDT contract

---
+++ test/ceres.js
@@ -0,0 +1,68 @@
+const BigNumber = require('bignumber.js');
+const util = require('util');
+const chalk = require('chalk');
+const Contract = require('web3-eth-contract');
+const { expectRevert, time } = require('@openzeppelin/test-helpers');
+
+// Set provider for all later instances to use
+Contract.setProvider('http://127.0.0.1:8545');
+
+global.artifacts = artifacts;
+global.web3 = web3;
+
+const Address = artifacts.require("Utils/Address");
+const BlockMiner = artifacts.require("Utils/BlockMiner");
+const Math = artifacts.require("Math/Math");
+const SafeMath = artifacts.require("Math/SafeMath");
+const Babylonian = artifacts.require("Math/Babylonian");
+const FixedPoint = artifacts.require("Math/FixedPoint");
+const UQ112x112 = artifacts.require("Math/UQ112x112");
+const Owned = artifacts.require("Staking/Owned");
+const ERC20 = artifacts.require("ERC20/ERC20");
+const ERC20Custom = artifacts.require("ERC20/ERC20Custom");
+const SafeERC20 = artifacts.require("ERC20/SafeERC20");
+
+// Uniswap related
+const TransferHelper = artifacts.require("Uniswap/TransferHelper");
+const SwapToPrice = artifacts.require("Uniswap/SwapToPrice");
+const UniswapV2ERC20 = artifacts.require("Uniswap/UniswapV2ERC20");
+const UniswapV2Factory = artifacts.require("Uniswap/UniswapV2Factory");
+const UniswapV2Library = artifacts.require("Uniswap/UniswapV2Library");
+const UniswapV2OracleLibrary = artifacts.require("Uniswap/UniswapV2OracleLibrary");
+const UniswapV2Pair = artifacts.require("Uniswap/UniswapV2Pair");
+const UniswapV2Router02_Modified = artifacts.require("Uniswap/UniswapV2Router02_Modified");
+
+// Collateral
+const WETH = artifacts.require("ERC20/WETH");
+const FakeCollateral_USDC = artifacts.require("FakeCollateral/FakeCollateral_USDC");
+const FakeCollateral_USDT = artifacts.require("FakeCollateral/FakeCollateral_USDT");
+const FakeCollateral_6DEC = artifacts.require("FakeCollateral/FakeCollateral_6DEC");
+
+
+
+contract('FRAX', async (accounts) => {
+	
+
+    beforeEach(async() => {
+		console.log("begin test");
+		console.log(chalk.red('====== Get Fake WETH & USDC & USDT ======='));
+		// Fill collateral instances
+		wethInstance = await WETH.deployed();
+		col_instance_USDC = await FakeCollateral_USDC.deployed(); 
+		col_instance_USDT = await FakeCollateral_USDT.deployed(); 
+		col_instance_6DEC = await FakeCollateral_6DEC.deployed();
+
+		console.log("wethInstance: ",wethInstance.address);
+		console.log("col_instance_USDC: ",col_instance_USDC.address);
+		console.log("col_instance_USDT: ",col_instance_USDT.address);
+		console.log("col_instance_6DEC: ",col_instance_6DEC.address);
+
+    });
+
+	// INITIALIZATION
+	// ================================================================
+	it('Check up on the oracles and make sure the prices are set', async () => {
+		console.log("Check up on the oracles and make sure the prices are set");
+	});
+
+});
