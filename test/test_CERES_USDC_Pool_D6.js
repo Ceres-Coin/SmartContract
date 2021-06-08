587a1cb3fca96accc432f2caf0d1da7dffede8e6
Ilya Prigogine <84952580+IlyaPrigogine@users.noreply.github.com>
2021-06-08 19:09:14 +0800

    Update test_CERES_USDC_Pool_D6.js
    finish test scripts of POOL_CEILING

--- test/test_CERES_USDC_Pool_D6.js
+++ test/test_CERES_USDC_Pool_D6.js
@@ -492,16 +492,16 @@ contract('CERES_USDC_Pool_D6', async (accounts) => {
 		console.log(chalk.blue("ER: pool_instance_USDC: ",pool_instance_USDC.address));
 		
 		// Before
-		// const er_missing_decimals = MISSING_DECIMALS;
+		const er_pool_ceiling = POOL_CEILING;
 		
 		// Action
 		const ar_pool_ceiling = await pool_instance_USDC.pool_ceiling.call();
 
 		// Assert
-		// assert.equal(ar_missing_decimals,er_missing_decimals,chalk.red.bold("ASSERTION FAILED"));
+		assert.equal(ar_pool_ceiling,er_pool_ceiling,chalk.red.bold("ASSERTION FAILED"));
 
 		// Print ER
-        // console.log(chalk.blue("er_missing_decimals: ",er_missing_decimals.toString()));
+        console.log(chalk.blue("er_pool_ceiling: ",er_pool_ceiling.toString()));
         
         // Print AR
 		console.log(chalk.yellow("ar_pool_ceiling: ",ar_pool_ceiling.toString()));
