f93d5bff5e342b70ffe0b226f1701cebb59d67ab
Ilya Prigogine <84952580+IlyaPrigogine@users.noreply.github.com>
2021-06-28 12:48:12 +0800

    add test scripts for time.advanceBlock() 3 times & 100 times

--- test/test_Openzeppelin_Test_Helpers.js
+++ test/test_Openzeppelin_Test_Helpers.js
@@ -47,6 +47,26 @@ contract('test_Openzeppelin_Test_Helpers', async (accounts) => {
 		// ASSERTION
 		expect(timestamp_after-timestamp_before).to.equal(ONE_MILLION);
 	});
+
+	it ("Test for time.advanceBlock() 3 times", async() => {
+		const THREE = 3;
+		const latestBlock_before = (new BigNumber(await time.latestBlock())).toNumber();
+		for (var i=0;i<THREE; i++) { await time.advanceBlock()};
+		const latestBlock_after = (new BigNumber(await time.latestBlock())).toNumber();
+
+		// ASSERTION
+		expect(latestBlock_after-latestBlock_before).to.equal(THREE);
+	});
+
+	it ("Test for time.advanceBlock() 100 times", async() => {
+		const ONE_HUNDRED = 100;
+		const latestBlock_before = (new BigNumber(await time.latestBlock())).toNumber();
+		for (var i=0;i<ONE_HUNDRED; i++) { await time.advanceBlock()};
+		const latestBlock_after = (new BigNumber(await time.latestBlock())).toNumber();
+
+		// ASSERTION
+		expect(latestBlock_after-latestBlock_before).to.equal(ONE_HUNDRED);
+	});
 });
 
 
