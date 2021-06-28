d82893e95f823b83404bf53521bfe6c1987d0a46
Ilya Prigogine <84952580+IlyaPrigogine@users.noreply.github.com>
2021-06-28 08:04:29 +0800

    add P1 test scripts

--- contracts/Ceres/Pools/CeresPool.sol
+++ contracts/Ceres/Pools/CeresPool.sol
@@ -140,8 +140,8 @@ contract CeresPool is AccessControl {
 
     // TODO: [FUNC][collatEthOracle_eth_collat_price]
     function collatEthOracle_eth_collat_price() public view returns(uint256) {
-        // uint256 eth_collat_price = collatEthOracle.consult(weth_address, (PRICE_PRECISION * (10 ** missing_decimals)));
-        uint256 eth_collat_price = collatEthOracle.consult(weth_address, PRICE_PRECISION);
+        uint256 eth_collat_price = collatEthOracle.consult(weth_address, (PRICE_PRECISION * (10 ** missing_decimals)));
+        // uint256 eth_collat_price = collatEthOracle.consult(weth_address, PRICE_PRECISION);
         return eth_collat_price;
     }
 
