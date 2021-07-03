28401b3fd0919cae780174dc6f8e6151d9f3d3c7
Ilya Prigogine <84952580+IlyaPrigogine@users.noreply.github.com>
2021-07-03 09:37:24 +0800

    ADDED: "Test Scripts for ceresInstance.pool_burn_from()"
    ADDED: "Test Scripts for ceresInstance.pool_burn_from()"

--- test/test_CERES_Contract_P3.js
+++ test/test_CERES_Contract_P3.js
@@ -29,6 +29,7 @@ const ERC20Custom = artifacts.require("ERC20/ERC20Custom");
 const SafeERC20 = artifacts.require("ERC20/SafeERC20");
 
 // set constants
+const ONE_HUNDRED_DEC18 = new BigNumber("100e18");
 const ONE_THOUSAND_DEC18 = new BigNumber("1000e18");
 const ONE_MILLION_DEC18 = new BigNumber("1000000e18");
 const FIVE_MILLION_DEC18 = new BigNumber("5000000e18");
@@ -269,6 +270,21 @@ contract('test_CERES_Contract_P3', async (accounts) => {
 		// ROLL BACK CODE
 		await ceresInstance.removePool(METAMASK_ADDRESS,{from: OWNER});
 	});
+
+	it ("Test Scripts for ceresInstance.pool_burn_from()",async() => {
+		const balanceOf_before = parseFloat(await ceresInstance.balanceOf(METAMASK_ADDRESS));
+		// console.log(chalk.yellow(`balanceOf_before: ${balanceOf_before}`));
+
+		await ceresInstance.addPool(METAMASK_ADDRESS,{from: OWNER});
+		await ceresInstance.pool_burn_from(METAMASK_ADDRESS,ONE_HUNDRED_DEC18,{from: METAMASK_ADDRESS});
+		
+		const balanceOf_after = parseFloat(await ceresInstance.balanceOf(METAMASK_ADDRESS));
+		// console.log(chalk.yellow(`balanceOf_after: ${balanceOf_after}`));
+		expect(balanceOf_after).to.equal(parseFloat(balanceOf_before-ONE_HUNDRED_DEC18));
+
+		// ROLL BACK CODE
+		await ceresInstance.removePool(METAMASK_ADDRESS,{from: OWNER});
+	})
 
 	it ("Test Scripts for ceresInstance.addPool() & removePool()", async() => {
 		const ceres_pools_OWNER_BEFORE = await ceresInstance.ceres_pools(OWNER);
