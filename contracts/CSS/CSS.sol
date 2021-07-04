96e06ea2b8591a3de80c1bc46f259bd51dc204ea
Ilya Prigogine <84952580+IlyaPrigogine@users.noreply.github.com>
2021-07-04 09:47:11 +0800

    ADDED: TEST SCRIPTS FOR cssInstance.CSS_DAO_min()
    ADDED: TEST SCRIPTS FOR cssInstance.CSS_DAO_min()

--- contracts/CSS/CSS.sol
+++ contracts/CSS/CSS.sol
@@ -20,7 +20,7 @@ contract CEREShares is ERC20Custom, AccessControl {
     address public CEREStablecoinAdd; //TEST CASE DONE
     
     uint256 public constant genesis_supply = 100000000e18; //TEST CASE DONE
-    uint256 public CSS_DAO_min; 
+    uint256 public CSS_DAO_min; //TEST CASE DONE
 
     address public owner_address;
     address public oracle_address;
