3b2d96924860df0aeea450d464e582151795085d
Ilya Prigogine <84952580+IlyaPrigogine@users.noreply.github.com>
2021-07-05 07:57:01 +0800

    Update test_6DEC_Tests.js

--- test/test_6DEC_Tests.js
+++ test/test_6DEC_Tests.js
@@ -437,16 +437,12 @@ contract('test_6DEC_Tests', async (accounts) => {
 	});
 
 	it("[mintFractionalCERES]: Mint some CERES using CSS and 6DEC (collateral ratio between .000001 and .999999)", async () => {
-		console.log("============6DEC mintFractionalCERES()============");
-		const totalSupplyCERES = new BigNumber(await ceresInstance.totalSupply.call()).div(BIG18).toNumber();
-		const totalSupplyCSS = new BigNumber(await cssInstance.totalSupply.call()).div(BIG18).toNumber();
+		console.log(chalk.blue("============USDC mintFractionalCERES()============"));
+		const totalSupplyCERES_before = new BigNumber(await ceresInstance.totalSupply.call()).div(BIG18).toNumber();
+		const totalSupplyCSS_before = new BigNumber(await cssInstance.totalSupply.call()).div(BIG18).toNumber();
 		const global_collateral_ratio = new BigNumber(await ceresInstance.global_collateral_ratio.call()).div(BIG6).toNumber();
 		const globalCollateralValue = new BigNumber(await ceresInstance.globalCollateralValue.call()).div(BIG18).toNumber();
 
-		// console.log("CERES price (USD): ", (new BigNumber(await ceresInstance.ceres_price.call()).div(BIG6)).toNumber());
-		// console.log("CSS price (USD): ", (new BigNumber(await ceresInstance.css_price.call()).div(BIG6)).toNumber());
-	
-		// CeresInstance.refreshCollateralRatio()
 		// Before
 		await ceresInstance.setRefreshCooldown(1,{from: OWNER});
 		// Action
@@ -465,43 +461,43 @@ contract('test_6DEC_Tests', async (accounts) => {
 		const pool_usdc_before = new BigNumber(await col_instance_USDC.balanceOf.call(pool_instance_USDC.address)).div(BIG6);
 		const collateral_price = (new BigNumber(await pool_instance_USDC.getCollateralPrice.call()).div(BIG6)).toNumber()
 
-		console.log(chalk.yellow(`ceres_before: ${ceres_before}`));
-		console.log(chalk.yellow(`css_before: ${css_before}`));
-		console.log(chalk.yellow(`usdc_before: ${usdc_before}`));
-		console.log(chalk.yellow(`pool_usdc_before: ${pool_usdc_before}`))
+		// COMMENT PRINT CODE
+		// COMMENT PRINT CODE
+		// COMMENT PRINT CODE
+		// console.log(chalk.yellow(`ceres_before: ${ceres_before}`));
+		// console.log(chalk.yellow(`css_before: ${css_before}`));
+		// console.log(chalk.yellow(`usdc_before: ${usdc_before}`));
+		// console.log(chalk.yellow(`pool_usdc_before: ${pool_usdc_before}`))
 
 		// ACTION
-		// const collateral_amount = ONE_HUNDRED_DEC6;
-		// await col_instance_USDC.approve(pool_instance_USDC.address, collateral_amount, { from: OWNER });
-		// const ceres_out_min = new BigNumber(collateral_amount.times(collateral_price).times(0.99)); // 1% slippage
-		// await pool_instance_USDC.mint1t1CERES(collateral_amount, ceres_out_min, { from: OWNER });
-
-		// Need to approve first so the pool contract can use transferFrom
 		const css_amount = new BigNumber(FIVE_HUNDRED_DEC18);
 		await cssInstance.approve(pool_instance_USDC.address, css_amount, { from: OWNER });
 		const usdc_amount = new BigNumber(ONE_HUNDRED_DEC6);
 		await col_instance_USDC.approve(pool_instance_USDC.address, usdc_amount, { from: OWNER });
 		await pool_instance_USDC.mintFractionalCERES(usdc_amount, css_amount, new BigNumber("10e18"), { from: OWNER });
-		console.log("accounts[0] mintFractionalCERES() with 100 6DEC and 500 CSS");
-
-
-
-
-
+		
 		const ceres_after = new BigNumber(await ceresInstance.balanceOf.call(OWNER)).div(BIG18);
 		const css_after = new BigNumber(await cssInstance.balanceOf.call(OWNER)).div(BIG18);
 		const usdc_after = new BigNumber(await col_instance_USDC.balanceOf.call(OWNER)).div(BIG6);
 		const pool_usdc_after = new BigNumber(await col_instance_USDC.balanceOf.call(pool_instance_USDC.address)).div(BIG6);
-		console.log(chalk.yellow(`ceres_after: ${ceres_after}`));
-		console.log(chalk.yellow(`css_after: ${css_after}`));
-		console.log(chalk.yellow(`usdc_after: ${usdc_after}`));
-		console.log(chalk.yellow(`pool_usdc_after: ${pool_usdc_after}`));
+		
+		const totalSupplyCERES_after = new BigNumber(await ceresInstance.totalSupply.call()).div(BIG18).toNumber();
+		const totalSupplyCSS_after = new BigNumber(await cssInstance.totalSupply.call()).div(BIG18).toNumber();
+		// COMMENT PRINT CODE
+		// COMMENT PRINT CODE
+		// COMMENT PRINT CODE
+		// console.log(chalk.yellow(`ceres_after: ${ceres_after}`));
+		// console.log(chalk.yellow(`css_after: ${css_after}`));
+		// console.log(chalk.yellow(`usdc_after: ${usdc_after}`));
+		// console.log(chalk.yellow(`pool_usdc_after: ${pool_usdc_after}`));
 
-
-		console.log("OWNER ceres change: ", ceres_after.toNumber() - ceres_before.toNumber());
-		console.log("OWNER css change: ", css_after.toNumber() - css_before.toNumber());
-		console.log("OWNER usdc change: ", usdc_after.toNumber() - usdc_before.toNumber());
-		console.log("pool_usdc_after usdc change: ", pool_usdc_after.toNumber() - pool_usdc_before.toNumber());
+		console.log(chalk.blue("ceres total supply change: ", totalSupplyCERES_after.toNumber() - totalSupplyCERES_before.toNumber()));
+		console.log(chalk.blue("css total supply change: ", totalSupplyCSS_after.toNumber() - totalSupplyCSS_before.toNumber()));
+		
+		console.log(chalk.yellow("OWNER ceres change: ", ceres_after.toNumber() - ceres_before.toNumber()));
+		console.log(chalk.yellow("OWNER css change: ", css_after.toNumber() - css_before.toNumber()));
+		console.log(chalk.yellow("OWNER usdc change: ", usdc_after.toNumber() - usdc_before.toNumber()));
+		console.log(chalk.yellow("pool_usdc_after usdc change: ", pool_usdc_after.toNumber() - pool_usdc_before.toNumber()));
 
 		// // Note the new collateral ratio
 		const global_collateral_ratio_after = new BigNumber(await ceresInstance.global_collateral_ratio.call()).div(BIG6);
