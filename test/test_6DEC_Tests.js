8ebc6485ad5dede809f2fcc19e3da525eed0d9c7
Ilya Prigogine <84952580+IlyaPrigogine@users.noreply.github.com>
2021-07-05 09:00:39 +0800

    Update test_6DEC_Tests.js

--- test/test_6DEC_Tests.js
+++ test/test_6DEC_Tests.js
@@ -501,8 +501,8 @@ contract('test_6DEC_Tests', async (accounts) => {
 
 		// // Note the new collateral ratio
 		const global_collateral_ratio_after = new BigNumber(await ceresInstance.global_collateral_ratio.call()).div(BIG6);
-		console.log(chalk.blue(`global_collateral_ratio_before: ${global_collateral_ratio_before}`));
-		console.log(chalk.blue("global_collateral_ratio_after: ", global_collateral_ratio_after.toNumber()));
+		// console.log(chalk.blue(`global_collateral_ratio_before: ${global_collateral_ratio_before}`));
+		// console.log(chalk.blue("global_collateral_ratio_after: ", global_collateral_ratio_after.toNumber()));
 	});
 
 
