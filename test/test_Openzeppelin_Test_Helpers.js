22162b39cd83cbf3f595b7b4e80e6881debcd760
Ilya Prigogine <84952580+IlyaPrigogine@users.noreply.github.com>
2021-07-05 10:48:03 +0800

    Update test_Openzeppelin_Test_Helpers.js
    ADDED TEST SCRIPTS OF erc20.transFrom

--- test/test_Openzeppelin_Test_Helpers.js
+++ test/test_Openzeppelin_Test_Helpers.js
@@ -12,6 +12,75 @@ Contract.setProvider('http://127.0.0.1:8545');
 global.artifacts = artifacts;
 global.web3 = web3;
 
+const BIG6 = new BigNumber("1e6");
+const BIG18 = new BigNumber("1e18");
+
+const Address = artifacts.require("Utils/Address");
+const BlockMiner = artifacts.require("Utils/BlockMiner");
+const Math = artifacts.require("Math/Math");
+const SafeMath = artifacts.require("Math/SafeMath");
+const Babylonian = artifacts.require("Math/Babylonian");
+const FixedPoint = artifacts.require("Math/FixedPoint");
+const UQ112x112 = artifacts.require("Math/UQ112x112");
+const Owned = artifacts.require("Staking/Owned");
+const ERC20 = artifacts.require("ERC20/ERC20");
+const ERC20Custom = artifacts.require("ERC20/ERC20Custom");
+const SafeERC20 = artifacts.require("ERC20/SafeERC20");
+
+// Uniswap related
+const TransferHelper = artifacts.require("Uniswap/TransferHelper");
+const SwapToPrice = artifacts.require("Uniswap/SwapToPrice");
+const UniswapV2ERC20 = artifacts.require("Uniswap/UniswapV2ERC20");
+const UniswapV2Factory = artifacts.require("Uniswap/UniswapV2Factory");
+const UniswapV2Library = artifacts.require("Uniswap/UniswapV2Library");
+const UniswapV2OracleLibrary = artifacts.require("Uniswap/UniswapV2OracleLibrary");
+const UniswapV2Pair = artifacts.require("Uniswap/UniswapV2Pair");
+const UniswapV2Router02_Modified = artifacts.require("Uniswap/UniswapV2Router02_Modified");
+
+// Collateral
+const WETH = artifacts.require("ERC20/WETH");
+const FakeCollateral_USDC = artifacts.require("FakeCollateral/FakeCollateral_USDC");
+const FakeCollateral_USDT = artifacts.require("FakeCollateral/FakeCollateral_USDT");
+const FakeCollateral_6DEC = artifacts.require("FakeCollateral/FakeCollateral_6DEC");
+
+// Core Contract CERES & CSS
+const CEREStable = artifacts.require("Ceres/CEREStable");
+const CEREShares = artifacts.require("CSS/CEREShares");
+
+// Timelock
+const Timelock = artifacts.require("Governance/Timelock");
+const TimelockTest = artifacts.require("Governance/TimelockTest");
+
+// Fake Oracle
+const UniswapPairOracle_CERES_WETH = artifacts.require("Oracle/Fakes/UniswapPairOracle_CERES_WETH");
+const UniswapPairOracle_CERES_USDC = artifacts.require("Oracle/Fakes/UniswapPairOracle_CERES_USDC");
+const UniswapPairOracle_CSS_WETH = artifacts.require("Oracle/Fakes/UniswapPairOracle_CSS_WETH");
+const UniswapPairOracle_CSS_USDC = artifacts.require("Oracle/Fakes/UniswapPairOracle_CSS_USDC");
+const UniswapPairOracle_USDC_WETH = artifacts.require("Oracle/Variants/UniswapPairOracle_USDC_WETH");
+const UniswapPairOracle = artifacts.require("Oracle/UniswapPairOracle");
+
+// ChainlinkETHUSD Contract
+const ChainlinkETHUSDPriceConsumerTest = artifacts.require("Oracle/ChainlinkETHUSDPriceConsumerTest");
+const ChainlinkETHUSDPriceConsumer = artifacts.require("Oracle/ChainlinkETHUSDPriceConsumer");
+
+const Pool_USDC = artifacts.require("Ceres/Pools/Pool_USDC");
+
+// set constants
+const ONE_HUNDRED_DEC18 = new BigNumber("100e18");
+const ONE_HUNDRED_DEC6 = new BigNumber("100e6");
+const FIVE_HUNDRED_DEC18 = new BigNumber("500e18");
+const ONE_THOUSAND_DEC18 = new BigNumber("1000e18");
+const ONE_MILLION_DEC18 = new BigNumber("1000000e18");
+const FIVE_MILLION_DEC18 = new BigNumber("5000000e18");
+const FIVE_MILLION_DEC6 = new BigNumber("5000000e6");
+const TEN_MILLION_DEC18 = new BigNumber("10000000e18");
+const ONE_HUNDRED_MILLION_DEC18 = new BigNumber("100000000e18");
+const ONE_HUNDRED_MILLION_DEC6 = new BigNumber("100000000e6");
+const ONE_BILLION_DEC18 = new BigNumber("1000000000e18");
+const COLLATERAL_SEED_DEC18 = new BigNumber(508500e18);
+const TWO_MILLION_DEC18 = (new BigNumber("20000000e18")).toNumber();
+const TWO_MILLION_DEC6 = (new BigNumber("20000000e6")).toNumber();
+
 // ADD TEST Scripts for @openzeppelin/test-helpers
 contract('test_Openzeppelin_Test_Helpers', async (accounts) => {
 	// deploy address;
@@ -282,9 +351,16 @@ contract('test_Openzeppelin_Test_Helpers', async (accounts) => {
 
 	it ("Test for ERC20.transFrom() func", async() => {
 		console.log(chalk.red(`====================== transFrom ================`));
-		await col_instance_USDC.approve(pool_instance_USDC.address, TWO_MILLION_DEC6, { from: OWNER });
+		const USDC_METAMASK_ADDRESS_BEFORE = await col_instance_USDC.balanceOf(METAMASK_ADDRESS);
+		console.log(chalk.yellow(`USDC_METAMASK_ADDRESS_BEFORE: ${USDC_METAMASK_ADDRESS_BEFORE}`));
 
-		
+		await col_instance_USDC.approve(pool_instance_USDC.address, TWO_MILLION_DEC6, { from: OWNER });
+		await col_instance_USDC.transferFrom(pool_instance_USDC.address,METAMASK_ADDRESS,ONE_HUNDRED_DEC6,{ from: OWNER });
+
+		const USDC_METAMASK_ADDRESS_AFTER = await col_instance_USDC.balanceOf(METAMASK_ADDRESS);
+		console.log(chalk.yellow(`USDC_METAMASK_ADDRESS_AFTER: ${USDC_METAMASK_ADDRESS_AFTER}`));
+
+
 	})
 	
 });
