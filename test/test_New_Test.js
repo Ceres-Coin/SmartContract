b827be871c20eae14d936cdcef01048a46938c0e
Ilya Prigogine <84952580+IlyaPrigogine@users.noreply.github.com>
2021-07-06 09:23:13 +0800

    Update test_New_Test.js

--- test/test_New_Test.js
+++ test/test_New_Test.js
@@ -390,7 +390,7 @@ contract('TEST SCRIPTS FOR test/test_New_Test.js', async (accounts) => {
 
 	it("Deploys a vesting contract and then executes a governance proposal to revoke it", async () => { 
 		console.log(chalk.blue("======== Setup vestingInstance ========"));
-		console.log(chalk.yellow(`vestingInstance: ${vestingInstance}`));
+		console.log(chalk.yellow(`vestingInstance: ${await vestingInstance.address}`));
 	});
 
 });
