e2e518e721c87956045db61afee06dc379f07deb
Ilya Prigogine <84952580+IlyaPrigogine@users.noreply.github.com>
2021-07-07 07:38:30 +0800

    Update test_Staking_CERES_WETH_P1.js

--- test/test_Staking_CERES_WETH_P1.js
+++ test/test_Staking_CERES_WETH_P1.js
@@ -359,13 +359,13 @@ contract('TEST SCRIPTS FOR test/test_New_Test.js', async (accounts) => {
 		const rewards_account0 = parseFloat(await stakingInstance_CERES_WETH.userRewardPerTokenPaid.call(account0));
 		console.log(chalk.yellow(`rewards_account0: ${account0} value: ${rewards_account0}`));
 
-		// console.log(chalk.yellow(`rewards_account1: ${account1} value: ${parseFloat(await stakingInstance_CERES_WETH.rewards.call(account1))}`));
-		// console.log(chalk.yellow(`rewards_account2: ${account2} value: ${parseFloat(await stakingInstance_CERES_WETH.rewards.call(account2))}`));
-		// console.log(chalk.yellow(`rewards_account3: ${account3} value: ${parseFloat(await stakingInstance_CERES_WETH.rewards.call(account3))}`));
-		// console.log(chalk.yellow(`rewards_account4: ${account4} value: ${parseFloat(await stakingInstance_CERES_WETH.rewards.call(account4))}`));
-		// console.log(chalk.yellow(`rewards_account5: ${account5} value: ${parseFloat(await stakingInstance_CERES_WETH.rewards.call(account5))}`));
-		// console.log(chalk.yellow(`rewards_account6: ${account6} value: ${parseFloat(await stakingInstance_CERES_WETH.rewards.call(account6))}`));
-		// console.log(chalk.yellow(`rewards_account7: ${account7} value: ${parseFloat(await stakingInstance_CERES_WETH.rewards.call(account7))}`));
+		console.log(chalk.yellow(`rewards_account1: ${account1} value: ${parseFloat(await stakingInstance_CERES_WETH.userRewardPerTokenPaid.call(account1))}`));
+		console.log(chalk.yellow(`rewards_account2: ${account2} value: ${parseFloat(await stakingInstance_CERES_WETH.userRewardPerTokenPaid.call(account2))}`));
+		console.log(chalk.yellow(`rewards_account3: ${account3} value: ${parseFloat(await stakingInstance_CERES_WETH.userRewardPerTokenPaid.call(account3))}`));
+		console.log(chalk.yellow(`rewards_account4: ${account4} value: ${parseFloat(await stakingInstance_CERES_WETH.userRewardPerTokenPaid.call(account4))}`));
+		console.log(chalk.yellow(`rewards_account5: ${account5} value: ${parseFloat(await stakingInstance_CERES_WETH.userRewardPerTokenPaid.call(account5))}`));
+		console.log(chalk.yellow(`rewards_account6: ${account6} value: ${parseFloat(await stakingInstance_CERES_WETH.userRewardPerTokenPaid.call(account6))}`));
+		console.log(chalk.yellow(`rewards_account7: ${account7} value: ${parseFloat(await stakingInstance_CERES_WETH.userRewardPerTokenPaid.call(account7))}`));
 	});
 
 
