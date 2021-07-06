566ddb4213238c2ea2f8af2267b19223061d6ca8
Ilya Prigogine <84952580+IlyaPrigogine@users.noreply.github.com>
2021-07-06 10:30:30 +0800

    Update test_New_Test.js
    added: "TEST SCRIPTS FOR TokenVesting.getStart()"

--- test/test_New_Test.js
+++ test/test_New_Test.js
@@ -430,6 +430,11 @@ contract('TEST SCRIPTS FOR test/test_New_Test.js', async (accounts) => {
 		expect(parseFloat(await vestingInstance.getCliff())).to.gt(0)
 	});
 
+	it ("TEST SCRIPTS FOR TokenVesting.getStart()", async() => {
+		console.log(chalk.yellow(`TokenVesting.getStart(): ${await TokenVesting.getStart()}`));
+		expect(parseFloat(await vestingInstance.getStart())).to.gt(0);
+	});
+
 });
 
 
