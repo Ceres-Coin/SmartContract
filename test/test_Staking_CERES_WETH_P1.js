3c153ed7e00073211029037e3f1d7e25b2979d1a
Ilya Prigogine <84952580+IlyaPrigogine@users.noreply.github.com>
2021-07-08 10:33:57 +0800

    Update test_Staking_CERES_WETH_P1.js

--- test/test_Staking_CERES_WETH_P1.js
+++ test/test_Staking_CERES_WETH_P1.js
@@ -387,7 +387,7 @@ contract('TEST SCRIPTS FOR test/test_New_Test.js', async (accounts) => {
 	});
 
 	it ("TEST SCRIPTS FOR stakingInstance_CERES_WETH.lockedStakesOf(account0)", async() => {
-		const value_account0 = await stakingInstance_CERES_WETH.lockedStakesOf.call(account0);
+		const value_account0 = (await stakingInstance_CERES_WETH.lockedStakesOf.call(account0)[0]);
 		console.log(chalk.yellow(`address_account0: ${account0} value: ${value_account0}`));
 	});
 
