8989a15ab3ad621f85f50f9b0f4ce807c58625ef
Ilya Prigogine <84952580+IlyaPrigogine@users.noreply.github.com>
2021-07-08 14:10:54 +0800

    Update test_Staking_CERES_WETH_P3.js

--- test/test_Staking_CERES_WETH_P3.js
+++ test/test_Staking_CERES_WETH_P3.js
@@ -296,10 +296,10 @@ contract('TEST SCRIPTS FOR test/test_Staking_CERES_WETH_P2.js', async (accounts)
 		expect(parseFloat(await stakingInstance_CERES_WETH.lastTimeRewardApplicable())).to.equal(DEFAULT_VALUE);
 	});
 
-	it ("TEST SCRIPTS FOR stakingInstance_CERES_WETH.rewardPerToken(), check its default value", async() => {
-		console.log(chalk.yellow(`rewardPerToken: ${parseFloat(await stakingInstance_CERES_WETH.rewardPerToken())}`));
-		// const DEFAULT_VALUE = 0;
-		// expect(parseFloat(await stakingInstance_CERES_WETH.lastTimeRewardApplicable())).to.equal(DEFAULT_VALUE);
+	it ("TEST SCRIPTS FOR stakingInstance_CERES_WETH.rewardPerToken(), check its default value is 0", async() => {
+		// console.log(chalk.yellow(`rewardPerToken: ${parseFloat(await stakingInstance_CERES_WETH.rewardPerToken())}`));
+		const DEFAULT_VALUE = 0;
+		expect(parseFloat(await stakingInstance_CERES_WETH.rewardPerToken())).to.equal(DEFAULT_VALUE);
 	});
 	
 
